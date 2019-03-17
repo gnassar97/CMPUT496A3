867884698ad6b6eab9a06aa537c3fb17b6880a21
zaheenrahman <zaheen@ualberta.ca>
2019-03-17 15:26:18 -0600

    Win/Random working

--- Gomoku3.py
+++ Gomoku3.py
@@ -68,7 +68,7 @@ class Gomoku():
             if gameCheck[1] == playerSimulationColor:
                 #print("CurrentPlayerWon, adding in win",gameCheck[1], color)
                 win_count += 1
-                
+
         while gameCheck[0] == False:
             legal_moves = GoBoardUtil.generate_legal_moves_gomoku(boardToSimulate)
             if len(legal_moves) == 0:
@@ -108,7 +108,7 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     #Default sim is 10. Otherwise can change it.
-    parser.add_argument('--sim', type=int, default=10, help='number of simulations per move, so total playouts=sim*legal_moves')
+    parser.add_argument('--sim', type=int, default=100, help='number of simulations per move, so total playouts=sim*legal_moves')
     parser.add_argument('--simrule', type=str, default='random', help='type of simulation policy: random or rulebased')
     args = parser.parse_args()
     sim = args.sim
